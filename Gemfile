167584f7e4c894098cbea31371ad5931e3c92280
Alan Potter <alanjpotter@gmail.com>
2016-06-23 11:44:26 -0400

    update test environment to 4.8, ignore features/reports

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby "2.0.0"
+#ruby=2.2.4
 #ruby-gemset=conjur-asset-authn-local
 
 # Specify your gem's dependencies in conjur-asset-authn-local.gemspec
--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-CONJUR_VERSION=${CONJUR_VERSION:-"4.6"}
+CONJUR_VERSION=${CONJUR_VERSION:-"4.8"}
 DOCKER_IMAGE=${DOCKER_IMAGE:-"registry.tld/conjur-appliance-cuke-master:$CONJUR_VERSION-stable"}
 NOKILL=${NOKILL:-"0"}
 PULL=${PULL:-"1"}
@@ -10,7 +10,7 @@ if [ -z "$CONJUR_CONTAINER" ]; then
 	    docker pull $DOCKER_IMAGE
 	fi
 	
-	cid=$(docker run -d -v ${PWD}:/src/conjur-asset-authn-local $DOCKER_IMAGE)
+	cid=$(docker run --privileged -d -v ${PWD}:/src/conjur-asset-authn-local $DOCKER_IMAGE)
 	function finish {
     	if [ "$NOKILL" != "1" ]; then
 			docker rm -f ${cid}
